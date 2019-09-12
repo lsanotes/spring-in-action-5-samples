cddae36e1721fe89069b42317d7295aba212fb1c
Liu Pei Pei <lsanotes@live.com>
2019-09-13 03:33:09 +0800

    https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/

--- ch04/tacos-jdbc/src/main/java/tacos/security/SecurityConfig.java
+++ ch04/tacos-jdbc/src/main/java/tacos/security/SecurityConfig.java
@@ -32,17 +32,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/design", "/orders")
-                .access("hasRole('ROLE_USER')")
-                .antMatchers("/", "/**").access("permitAll")
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/design", true)
-                .and()
-                .logout()
-                .logoutSuccessUrl("/");
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/design", "/orders")
+//                .access("hasRole('ROLE_USER')")
+//                .antMatchers("/", "/**").access("permitAll")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/design")
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/");
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").hasRole("ADMIN")//allow h2 console access to admins only
+                .anyRequest().authenticated()//all other urls can be access by any authenticated role
+                .and().formLogin()//enable form login instead of basic login
+                .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
+                .and().headers().frameOptions().sameOrigin();//allow use of frame to same origin urls
     }
 }
